124a8e0fba3b035d02fb21d2930c1c027dc3b364
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2022-08-11 19:33:30 +0300

    Step 2 done with refactor

--- hexlet-code/gendiff/scripts/gendiff.py
+++ hexlet-code/gendiff/scripts/gendiff.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser(description="Compares two configuration files a
 parser.add_argument("first_file")
 parser.add_argument("second_file")
 
+parser.add_argument("-f", "--format", help="set format of output")
+
 args = parser.parse_args()
 
 def main():
